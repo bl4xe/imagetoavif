169daaffa2712c338655fcdf058f4c48cfb8efa2
Dario <96691738+bl4xe@users.noreply.github.com>
2024-09-15 14:14:19 +0200

    Update buh.sh

--- buh.sh
+++ buh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the source and destination directories
-source_dir="/home/bl4ze/Downloads/Camera-Roll"
-dest_dir="/home/bl4ze/Downloads/zuh"
+source_dir="/home/bl4ze/Downloads/immich1"
+dest_dir="/home/bl4ze/Downloads/immichavif"
 
 # Function to convert images to AVIF
 convert_image() {
@@ -11,7 +11,7 @@ convert_image() {
     new_file="${dest_dir}/${base_name%.*}.avif"
 
     # Check if the file is an image
-    if [[ -f "$file" ]] && [[ -r "$file" ]] && [[ "$file" =~ \.(jpg|jpeg|png|webp|gif|bmp|tiff)$ ]]; then
+    if [[ -f "$file" ]] && [[ -r "$file" ]] && [[ "$file" =~ \.(JPG|jpg|JPEG|jpeg|png|webp|gif|bmp|tiff)$ ]]; then
         echo "Converting image to AVIF: $file"
         magick "$file" -set EXIF:* c -define formats:write_exif=true -quality 100 -colorspace sRGB "$new_file"
 
@@ -27,7 +27,7 @@ convert_video() {
     new_file="${dest_dir}/${base_name%.*}.mkv"
 
     # Check if the file is a video
-    if [[ -f "$file" ]] && [[ -r "$file" ]] && [[ "$file" =~ \.(mp4|mov|avi|mkv|flv|mpeg|mpg)$ ]]; then
+    if [[ -f "$file" ]] && [[ -r "$file" ]] && [[ "$file" =~ \.(mp4|MOV|mov|avi|mkv|flv|mpeg|mpg)$ ]]; then
         echo "Converting video to Matroshka: $file"
         ffmpeg -i "$file" -c:v libaom-av1 -crf 23 -c:a aac -b:a 128k -movflags faststart -metadata title="My Video Title" -metadata artist="John Doe" "$new_file"
 
@@ -41,14 +41,14 @@ find "$source_dir" -type f -print0 | while IFS= read -r -d '' file; do
     echo "Processing file: $file"
 
     # Check if the file is an image or video
-    if [[ "$file" =~ \.(jpg|jpeg|png|webp|gif|bmp|tiff|mp4|mov|avi|mkv|flv|mpeg|mpg)$ ]]; then
+    if [[ "$file" =~ \.(JPG|jpg|JPEG|jpeg|png|webp|gif|bmp|tiff|mp4|MOV|mov|avi|mkv|flv|mpeg|mpg)$ ]]; then
         # Convert the file to AVIF or Matroshka based on its type
-        if [[ "$file" =~ \.(jpg|jpeg|png|webp|gif|bmp|tiff)$ ]]; then
+        if [[ "$file" =~ \.(JPG|jpg|JPEG|jpeg|png|webp|gif|bmp|tiff)$ ]]; then
             echo "Converting image: $file"
             convert_image "$file"
-        elif [[ "$file" =~ \.(mp4|mkv|mov)$ ]]; then
+        elif [[ "$file" =~ \.(mp4|mkv|mov|MOV)$ ]]; then
             echo "Converting video: $file"
-            convert_video "$file"
+            # convert_video "$file"
         else
             echo "Don't know this file type: $file"
             echo "This script checks for jpg, jpeg, png, webp, gif, bmp, tiff for images"
